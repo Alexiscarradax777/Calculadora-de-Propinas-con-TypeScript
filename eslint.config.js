cf1fdc816591be2d519a2f35f94e4bc132243bc1
Alexis Carrada <carradaalexis551@gmail.com>
2025-10-23 17:04:44 -0600

    Modificando el archivo eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,31 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-const ReactCompilerConfig = {};
-
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
       },
-    }),
-  ],
-});
+    },
+  },
+]);
